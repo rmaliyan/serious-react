3d690432ebae8623162486459d7329623302db71
rmaliyan <rmaliyan14@gmail.com>
2023-12-01 16:35:51 +0400

    Made a second branch

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -17,7 +17,7 @@ function MenuSubitem({ content, url, menuOpen, setmenuOpen }) {
 
 function MenuSubitemLargeScreen({ url }) {
   return (
-    <Link className="ml-1 w-full" to={url}>
+    <Link className="px-1 w-full" to={url}>
       <div className="h-1 w-full bg-white "></div>
     </Link>
   );
@@ -31,30 +31,68 @@ function MenuItem({
   menuOpen,
   setmenuOpen,
 }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const [isScreenBiggerThan1024, setIsScreenBiggerThan1024] = useState(false);
-
-  // useEffect(() => {
-  //   const checkScreenSize = () => {
-  //     setIsScreenBiggerThan1024(window.innerWidth > 1024);
-  //   };
-
-  //   window.addEventListener('resize', checkScreenSize);
-  //   checkScreenSize();
-  //   return () => {
-
-  //     window.removeEventListener('resize', checkScreenSize);
-  //   };
-
-  // }, []);
-
+  const [isOpen, setIsOpen] = useState(false); 
+  
   return (
-    <div className="flex flex-col overflow-hidden border border-slate-500 lg:mx-2 lg:flex lg:content-center lg:justify-center lg:border-0">
-      <div className="flex w-full justify-between pr-5 lg:items-center lg:justify-center lg:p-0">
+    <div className="flex flex-col overflow-hidden border border-slate-500 lg:flex lg:content-center lg:justify-center lg:border-0">
+      <div className="flex w-full justify-between pr-5 lg:mx-2 lg:items-center lg:justify-center lg:p-0">
         <Link
           onClick={() => setmenuOpen(!menuOpen)}
           className="py-2 pl-7 lg:flex lg:p-0 "
+          to={url}
+        >
+          {langContent[content]}
+        </Link>
+
+        {subitems.length > 0 && (
+          <img
+            onClick={() => setIsOpen(!isOpen)}
+            className="h-4 lg:hidden"
+            src={isOpen ? "/lineUp.svg" : "/lineDown.svg"}
+            alt="Sidebar icon"
+          />
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="flex animate-slideInText flex-col lg:hidden">
+          {subitems.map((child) => (
+            <MenuSubitem
+              menuOpen={menuOpen}
+              setmenuOpen={setmenuOpen}
+              content={langContent[child.content]}
+              url={child.url}
+              key={child.url}
+            />
+          ))}
+        </div>
+      )}
+
+      <div className="hidden lg:ml-1 lg:flex lg:w-4/5 lg:content-between">
+        {subitems.map((child) => (
+          <MenuSubitemLargeScreen url={child.url} key={child.url} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function MenuItemLargeScreen({
+  content,
+  url,
+  subitems,
+  langContent,
+  menuOpen,
+  setmenuOpen,
+}) {
+  const [isOpen, setIsOpen] = useState(false);  
+
+  return (
+    <div className="flex flex-col border-0 content-center justify-center">
+      <div className="flex :mx-2 items-center justify-center p-0">
+        <Link
+          onClick={() => setmenuOpen(!menuOpen)}
+          className="flex p-0"
           to={url}
         >
           {langContent[content]}
@@ -104,7 +142,8 @@ function Menu({ langContent, menuOpen, setmenuOpen }) {
         menuOpen ? "animate-slideInText" : "animate-slideOutText"
       } absolute right-0 top-24 h-fit w-full text-2xl font-extrabold backdrop-blur-sm lg:right-20 lg:top-6 lg:ml-auto lg:mr-4 lg:flex lg:h-fit lg:w-fit lg:gap-y-0.5 lg:pt-0 lg:text-base lg:font-normal lg:backdrop-blur-none `}
     >
-      {MenuItemsList.map((item) => (
+      {!menuOpen ?       
+       (MenuItemsList.map((item) => (
         <MenuItem
           menuOpen={menuOpen}
           setmenuOpen={setmenuOpen}
@@ -114,7 +153,22 @@ function Menu({ langContent, menuOpen, setmenuOpen }) {
           langContent={langContent}
           key={item.url}
         />
-      ))}
+      )))   
+      :
+      (MenuItemsList.map((item) => (
+        <MenuItemLargeScreen
+          menuOpen={menuOpen}
+          setmenuOpen={setmenuOpen}
+          content={item.content}
+          url={item.url}
+          subitems={item.subitems}
+          langContent={langContent}
+          key={item.url}
+        />
+      ))) 
+      }
+
+    
       <div className="mt-10 flex w-full justify-around lg:hidden">
         <Link
           onClick={() => setmenuOpen(!menuOpen)}
@@ -159,40 +213,40 @@ export function Layout() {
   const [menuOpen, setmenuOpen] = useState(false);
   const [x, setmenuX] = useState(false);
 
-  // function getWindowDimensions() {
-  //   const { innerWidth: width, innerHeight: height } = window;
-  //   return {
-  //     width,
-  //     height,
-  //   };
-  // }
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
 
-  // function useWindowDimensions() {
-  //   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
-  //   useEffect(() => {
-  //     function handleResize() {
-  //       setWindowDimensions(getWindowDimensions());
-  //     }
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
 
-  //     window.addEventListener("resize", handleResize);
-  //     return () => window.removeEventListener("resize", handleResize);
-  //   }, []);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
-  //   return windowDimensions;
-  // }
+    return windowDimensions;
+  }
 
-  // const { width } = useWindowDimensions();
-  // console.log( width);
-  // if(width >= 1024 && x == false){
-  //   setmenuOpen(true);
-  //   setmenuX(true)
-  // }else if(width < 1024 && x == true){
-  //       setmenuOpen(false);
-  //       setmenuX(false);
-  // }
+  const { width } = useWindowDimensions();
+  console.log( width);
+  if(width >= 1024 && x == false){
+    setmenuOpen(true);
+    setmenuX(true)
+  }else if(width < 1024 && x == true){
+        setmenuOpen(false);
+        setmenuX(false);
+  }
 
-  // console.log("menuOpen - " + menuOpen + ', x - ' + x);
+  console.log("menuOpen - " + menuOpen + ', x - ' + x);
 
   return (
     <div
@@ -236,7 +290,7 @@ export function Layout() {
         </div>
       </div>
 
-        {/* <div className="lower-bar flex h-fit  w-full">
+        <div className="lower-bar flex h-fit  w-full">
           <div className="outlet-container box-content h-screen w-full border border-slate-500 bg-slate-950 bg-opacity-50">
             <Outlet />
           </div>
@@ -252,7 +306,7 @@ export function Layout() {
               Arm
             </Link>
           </div>
-        </div> */}
+        </div>
     </div>
   );
 }
