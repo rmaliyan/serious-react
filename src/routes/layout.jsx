a6314d28a7719d731e122e25423866c732d20b77
rmaliyan <rmaliyan14@gmail.com>
2023-12-01 21:49:47 +0400

    Various fixes for menu

--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -7,21 +7,22 @@ function MenuSubitem({ content, url, menuOpen, setmenuOpen }) {
   return (
     <Link
       onClick={() => setmenuOpen(!menuOpen)}
-      className="ml-16 grow"
+      className="w-full lg:px-1"
       to={url}
     >
-      <div>{content}</div>
+      <div className="hidden lg:h-1 lg:w-full lg:bg-white lg:block"></div>
+      <div className ="lg:hidden">{content}</div>
     </Link>
   );
 }
 
-function MenuSubitemLargeScreen({ url }) {
-  return (
-    <Link className="px-1 w-full" to={url}>
-      <div className="h-1 w-full bg-white "></div>
-    </Link>
-  );
-}
+// function MenuSubitemLargeScreen({ url }) {
+//   return (
+//     <Link className="w-full px-1" to={url}>
+//       <div className="h-1 w-full bg-white "></div>
+//     </Link>
+//   );
+// }
 
 function MenuItem({
   content,
@@ -31,11 +32,11 @@ function MenuItem({
   menuOpen,
   setmenuOpen,
 }) {
-  const [isOpen, setIsOpen] = useState(false); 
-  
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex flex-col overflow-hidden border border-slate-500 lg:flex lg:content-center lg:justify-center lg:border-0">
-      <div className="flex w-full justify-between pr-5 lg:mx-2 lg:items-center lg:justify-center lg:p-0">
+    <div className="flex flex-col overflow-hidden border border-slate-500 lg:flex lg:items-center lg:justify-start lg:border-0">
+      <div className="flex w-full items-center justify-between pr-5 lg:mx-2 lg:items-start lg:justify-center lg:p-0">
         <Link
           onClick={() => setmenuOpen(!menuOpen)}
           className="py-2 pl-7 lg:flex lg:p-0 "
@@ -54,8 +55,8 @@ function MenuItem({
         )}
       </div>
 
-      {isOpen && (
-        <div className="flex animate-slideInText flex-col lg:hidden">
+      
+        <div className={`${isOpen ? "flex" : "hidden"} pl-16 w-full animate-slideInText flex-col lg:pl-0 lg:flex-row lg:flex`}>
           {subitems.map((child) => (
             <MenuSubitem
               menuOpen={menuOpen}
@@ -66,67 +67,7 @@ function MenuItem({
             />
           ))}
         </div>
-      )}
-
-      <div className="hidden lg:ml-1 lg:flex lg:w-4/5 lg:content-between">
-        {subitems.map((child) => (
-          <MenuSubitemLargeScreen url={child.url} key={child.url} />
-        ))}
-      </div>
-    </div>
-  );
-}
-
-function MenuItemLargeScreen({
-  content,
-  url,
-  subitems,
-  langContent,
-  menuOpen,
-  setmenuOpen,
-}) {
-  const [isOpen, setIsOpen] = useState(false);  
-
-  return (
-    <div className="flex flex-col border-0 content-center justify-center">
-      <div className="flex :mx-2 items-center justify-center p-0">
-        <Link
-          onClick={() => setmenuOpen(!menuOpen)}
-          className="flex p-0"
-          to={url}
-        >
-          {langContent[content]}
-        </Link>
-
-        {subitems.length > 0 && (
-          <img
-            onClick={() => setIsOpen(!isOpen)}
-            className="h-4 lg:hidden"
-            src={isOpen ? "/lineUp.svg" : "/lineDown.svg"}
-            alt="Sidebar icon"
-          />
-        )}
-      </div>
-
-      {isOpen && (
-        <div className="flex animate-slideInText flex-col lg:hidden">
-          {subitems.map((child) => (
-            <MenuSubitem
-              menuOpen={menuOpen}
-              setmenuOpen={setmenuOpen}
-              content={langContent[child.content]}
-              url={child.url}
-              key={child.url}
-            />
-          ))}
-        </div>
-      )}
-
-      <div className="hidden lg:ml-1 lg:flex lg:w-4/5 lg:content-between">
-        {subitems.map((child) => (
-          <MenuSubitemLargeScreen url={child.url} key={child.url} />
-        ))}
-      </div>
+           
     </div>
   );
 }
@@ -139,37 +80,24 @@ function Menu({ langContent, menuOpen, setmenuOpen }) {
   return (
     <div
       className={`${
-        menuOpen ? "animate-slideInText" : "animate-slideOutText"
-      } absolute right-0 top-24 h-fit w-full text-2xl font-extrabold backdrop-blur-sm lg:right-20 lg:top-6 lg:ml-auto lg:mr-4 lg:flex lg:h-fit lg:w-fit lg:gap-y-0.5 lg:pt-0 lg:text-base lg:font-normal lg:backdrop-blur-none `}
+        menuOpen ? "animate-slideInText" : "animate-slideOutText translate-x-full h-0"
+      } absolute right-0 top-24 h-full w-full text-2xl font-extrabold backdrop-blur-sm lg:right-0 
+      lg:top-0 lg:ml-auto lg:mr-4 lg:flex lg:w-fit lg:h-fit lg:gap-y-0.5 lg:pt-0 lg:text-base lg:font-normal 
+      lg:backdrop-blur-none lg:animate-none lg:translate-x-0 lg:relative`}
     >
-      {!menuOpen ?       
-       (MenuItemsList.map((item) => (
-        <MenuItem
-          menuOpen={menuOpen}
-          setmenuOpen={setmenuOpen}
-          content={item.content}
-          url={item.url}
-          subitems={item.subitems}
-          langContent={langContent}
-          key={item.url}
-        />
-      )))   
-      :
-      (MenuItemsList.map((item) => (
-        <MenuItemLargeScreen
-          menuOpen={menuOpen}
-          setmenuOpen={setmenuOpen}
-          content={item.content}
-          url={item.url}
-          subitems={item.subitems}
-          langContent={langContent}
-          key={item.url}
-        />
-      ))) 
-      }
+      {MenuItemsList.map((item) => (
+            <MenuItem
+              menuOpen={menuOpen}
+              setmenuOpen={setmenuOpen}
+              content={item.content}
+              url={item.url}
+              subitems={item.subitems}
+              langContent={langContent}
+              key={item.url}
+            />
+          ))}
 
-    
-      <div className="mt-10 flex w-full justify-around lg:hidden">
+      <div className="pt-10 flex w-full h-full justify-around border border-slate-500 lg:hidden">
         <Link
           onClick={() => setmenuOpen(!menuOpen)}
           className="my-2"
@@ -196,12 +124,6 @@ function Menu({ langContent, menuOpen, setmenuOpen }) {
   );
 }
 
-
-
-
-
-
-
 export function Layout() {
   const { lang } = useParams();
   const menu = Content[lang].menu;
@@ -211,6 +133,7 @@ export function Layout() {
   const currentLocation = location.pathname.substring(index + 1);
 
   const [menuOpen, setmenuOpen] = useState(false);
+
   const [x, setmenuX] = useState(false);
 
   function getWindowDimensions() {
@@ -222,7 +145,9 @@ export function Layout() {
   }
 
   function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    const [windowDimensions, setWindowDimensions] = useState(
+      getWindowDimensions(),
+    );
 
     useEffect(() => {
       function handleResize() {
@@ -237,21 +162,21 @@ export function Layout() {
   }
 
   const { width } = useWindowDimensions();
-  console.log( width);
-  if(width >= 1024 && x == false){
-    setmenuOpen(true);
-    setmenuX(true)
-  }else if(width < 1024 && x == true){
-        setmenuOpen(false);
-        setmenuX(false);
-  }
 
-  console.log("menuOpen - " + menuOpen + ', x - ' + x);
+  useEffect(() => {
+    if (width >= 1024) {
+      setmenuOpen(true);
+    } else if (width < 1024) {
+      setmenuOpen(false);
+    }
+  }, [width]);
+
+  console.log("menuOpen - " + menuOpen);
 
   return (
     <div
       id="layout-container"
-      className="layout-container flex h-fit w-full flex-col overflow-scroll text-white lg:overflow-hidden"
+      className="layout-container flex h-screen w-full flex-col overflow-x-hidden overflow-y-scroll text-white lg:overflow-hidden"
     >
       <div id="upper-bar" className="flex w-full">
         <div
@@ -269,13 +194,14 @@ export function Layout() {
             />
           </div>
 
-          {menuOpen && (
-            <Menu
-              menuOpen={menuOpen}
-              setmenuOpen={setmenuOpen}
-              langContent={menu}
-            />
-          )}
+          
+          <Menu 
+            menuOpen={menuOpen}
+            setmenuOpen={setmenuOpen}
+            langContent={menu}
+          />
+          
+
         </div>
 
         <div
@@ -290,23 +216,23 @@ export function Layout() {
         </div>
       </div>
 
-        <div className="lower-bar flex h-fit  w-full">
-          <div className="outlet-container box-content h-screen w-full border border-slate-500 bg-slate-950 bg-opacity-50">
-            <Outlet />
-          </div>
-
-          <div className="lower-vertical-bar hidden h-full w-20 flex-col items-center border-0 bg-transparent bg-opacity-50 sm:border-slate-500 sm:bg-slate-950 lg:flex">
-            <Link className="my-2 mt-10" to={`/en/${currentLocation}`}>
-              Eng
-            </Link>
-            <Link className="my-2" to={`/ru/${currentLocation}`}>
-              Rus
-            </Link>
-            <Link className="my-2" to={`/am/${currentLocation}`}>
-              Arm
-            </Link>
-          </div>
+      <div className="lower-bar flex h-full w-full">
+        <div className="outlet-container box-content h-full w-full border border-slate-500 bg-slate-950 bg-opacity-50">
+          <Outlet />
         </div>
+
+        <div className="lower-vertical-bar hidden h-full w-20 flex-col items-center border-0 bg-transparent bg-opacity-50 lg:place-self-stretch lg:border lg:border-slate-500 lg:bg-slate-950 lg:flex">
+          <Link className="my-2 mt-10" to={`/en/${currentLocation}`}>
+            Eng
+          </Link>
+          <Link className="my-2" to={`/ru/${currentLocation}`}>
+            Rus
+          </Link>
+          <Link className="my-2" to={`/am/${currentLocation}`}>
+            Arm
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
